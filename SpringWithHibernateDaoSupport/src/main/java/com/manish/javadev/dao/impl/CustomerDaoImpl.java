f3f98355762e6e0e317582d3524444d21f6feb2e
m.d.srivastava <m.d.srivastava@BDC7-D-434RPDM.dir.svc.accenture.com>
2015-09-15 21:56:35 +0530

    No Session problem resolved

--- SpringWithHibernateDaoSupport/src/main/java/com/manish/javadev/dao/impl/CustomerDaoImpl.java
+++ SpringWithHibernateDaoSupport/src/main/java/com/manish/javadev/dao/impl/CustomerDaoImpl.java
@@ -12,33 +12,37 @@ import com.manish.javadev.dao.CustomerDAO;
 import com.manish.javadev.model.Customer;
 import com.manish.javadev.to.CustomerTO;
 
-@SuppressWarnings("unchecked")
+@SuppressWarnings({"unchecked","rawtypes","unused"})
 public class CustomerDaoImpl extends HibernateDaoSupport implements CustomerDAO {
 
 	// Add Customer
 	public void addCustomer(final CustomerTO cto) {
-		Customer cust = (Customer)getHibernateTemplate().execute(new HibernateCallback() {
-			public Customer doInHibernate(Session session)
-					throws HibernateException {
-				Customer cust = new Customer(cto.getFirstName(), cto
-						.getLastName(), cto.getAge(), cto.getCity());
+		
+		Customer cust = (Customer) getHibernateTemplate().execute(
+				new HibernateCallback() {
+					public Customer doInHibernate(Session session)
+							throws HibernateException {
+						Customer cust = new Customer(cto.getFirstName(), cto
+								.getLastName(), cto.getAge(), cto.getCity());
 
-				session.saveOrUpdate(cust);
-				return cust;
-			}
-		});
+						session.saveOrUpdate(cust);
+						return cust;
+					}
+				});
 
 	}
 
 	// Update Customer By Id
 	public void updateCustomer(int custId) {
-
+		Customer cust = getHibernateTemplate().load(Customer.class, custId);
+		cust.setFirstName("Updated FirstName");
+		getHibernateTemplate().update(cust);
 	}
 
 	// Delete Customer By Id
 	public void deleteCustomer(int custId) {
-
-	
+		Customer cust = getHibernateTemplate().load(Customer.class, custId);
+		getHibernateTemplate().delete(cust);
 	}
 
 	// Get All Customer
@@ -57,7 +61,11 @@ public class CustomerDaoImpl extends HibernateDaoSupport implements CustomerDAO 
 
 	// Get Customer By Id
 	public CustomerTO getCustomerById(int custId) {
-		return null;
-		
+		Customer cust = getHibernateTemplate().load(Customer.class, custId);
+		CustomerTO custTo = new CustomerTO(cust.getCustId(),
+				cust.getFirstName(), cust.getLastName(), cust.getAge(),
+				cust.getCity());
+		return custTo;
+
 	}
 }
